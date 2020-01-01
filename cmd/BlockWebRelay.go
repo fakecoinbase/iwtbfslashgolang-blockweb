d5e598ed4d716eeb206d64def3f28120f370ef20
Timon Borter <timon.borter@gmx.ch>
2020-01-01 22:07:42 +0100

    update copyright notice to 2020.

--- cmd/BlockWebRelay.go
+++ cmd/BlockWebRelay.go
@@ -1,7 +1,7 @@
 package cmd
 
 /*
- * Copyright 2019 Information Wants To Be Free
+ * Copyright 2020 Information Wants To Be Free
  * Visit: https://github.com/iwtbf
  *
  * This project is licensed under the terms of the Apache 2.0 License.
--- cmd/blockweb-relay.go
+++
@@ -1,8 +0,0 @@
-package cmd
-
-/**
- * Copyright 2019 Information Wants To Be Free
- * Visit: https://github.com/iwtbf
- *
- * This project is licensed under the terms of the Apache 2.0 License.
- */
